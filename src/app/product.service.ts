848d31ad44a45c65fbe6b1a48b452c9a308cfcdf
Gopi Krishna Swargam <gkswargam@gmail.com>
2017-12-29 19:46:49 -0600

    Corretced error in Module6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -21,6 +21,6 @@ export class ProductService {
 
   getProducts() :Observable<Product[]>{
     return this._http.get(this._productsUrl)
-      .map((response) => response.json());
+      .map((response) => <Product[]>response.json());
   }
 }
